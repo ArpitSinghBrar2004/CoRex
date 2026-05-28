2b54802f8a09c57032a83216e304004c37f7ada7
Arpit Singh Brar <arpitsinghbrar853@gmail.com>
2026-05-28 14:25:16 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -21,8 +21,12 @@ else:
         async def setup_hook(self):
             await self.load_extension("cogs.utility")
             await self.load_extension("cogs.welcome")
+            await self.load_extension("cogs.left")
+            await self.load_extension("cogs.logs")
             print("Utility cog loaded")
             print("Welcome cog loaded")
+            print("Left cog loaded")
+            print("Logs cog loaded")
             synced = await self.tree.sync()
             print(f"Synced {len(synced)} slash commands")
 
